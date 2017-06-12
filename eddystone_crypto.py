da7572d42980ead35b5e07c4346e69674df32517
Mike Walters <mike@flomp.net>
2017-06-12 02:42:05 +0100

    Wording

--- eddystone_crypto.py
+++ eddystone_crypto.py
@@ -16,7 +16,7 @@ def compute_ik(shared_secret, service_public_key, beacon_public_key):
     ik = hkdf.hkdf_expand(prk, b"", 32, hash=hashlib.sha256)[:16]
     return ik
 
-def gen_tk(ik, counter):
+def compute_tk(ik, counter):
     tk_data = bytes([
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0xFF,
@@ -27,7 +27,7 @@ def gen_tk(ik, counter):
     tk = AES.new(ik, AES.MODE_ECB).encrypt(tk_data)
     return tk
 
-def gen_eid(ik, k, counter):
+def compute_eid(ik, k, counter):
     # Clear lower K bits
     counter = (counter >> k) << k
     tk = gen_tk(ik, counter)
