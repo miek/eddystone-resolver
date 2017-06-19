83f017e972db8d95cf5706a5092251bba4ccfa39
Mike Walters <mike@flomp.net>
2017-06-19 01:03:41 +0100

    Revert "Temp debug"
    This reverts commit 1097ea0485bdf6718b7165d7d9c2e53663e54ad5.

--- eddystone_crypto.py
+++ eddystone_crypto.py
@@ -8,20 +8,13 @@ def get_byte(num, i):
     return (num >> (24 - i*8)) & 0xFF
 
 def compute_shared_secret(priv, pub):
-    print(b'pub: ' + binascii.hexlify(pub))
-    print(b'priv: ' + binascii.hexlify(priv))
     secret = nacl.bindings.crypto_scalarmult(priv, pub)
-    print(b'secret: ' + binascii.hexlify(secret))
     return secret
 
 def compute_ik(shared_secret, service_public_key, beacon_public_key):
     salt = service_public_key + beacon_public_key
     prk = hkdf.hkdf_extract(salt, shared_secret, hash=hashlib.sha256)
     ik = hkdf.hkdf_expand(prk, b"", 32, hash=hashlib.sha256)[:16]
-    print(b'shared: ' + binascii.hexlify(shared_secret))
-    print(b'service: ' + binascii.hexlify(service_public_key))
-    print(b'beacon: ' + binascii.hexlify(beacon_public_key))
-    print(b'ik: ' + binascii.hexlify(ik))
     return ik
 
 def compute_tk(ik, counter):
@@ -33,7 +26,6 @@ def compute_tk(ik, counter):
         get_byte(counter, 1),
     ])
     tk = AES.new(ik, AES.MODE_ECB).encrypt(tk_data)
-    print(binascii.hexlify(tk))
     return tk
 
 def compute_eid(ik, k, counter):
