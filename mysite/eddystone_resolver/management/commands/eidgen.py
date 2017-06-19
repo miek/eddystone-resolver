c49d62ce35b45b7e0c907fd5182f3108d6e6695c
Mike Walters <mike@flomp.net>
2017-06-20 00:05:13 +0100

    Generate previous, current and next EIDs

--- mysite/eddystone_resolver/management/commands/eidgen.py
+++ mysite/eddystone_resolver/management/commands/eidgen.py
@@ -1,8 +1,11 @@
-from django.core.management.base import BaseCommand, CommandError
-from eddystone_resolver.models import Beacon, EID
+from django.core.management.base import BaseCommand
+from eddystone_resolver.models import Beacon
 
 class Command(BaseCommand):
     help = 'Generate EIDs for registered beacons'
 
     def handle(self, *args, **options):
-        self.stdout.write("hi")
+        for beacon in Beacon.objects.all():
+            beacon.eid_set.get_or_create(clock=beacon.counter_prev())
+            beacon.eid_set.get_or_create(clock=beacon.counter())
+            beacon.eid_set.get_or_create(clock=beacon.counter_next())
