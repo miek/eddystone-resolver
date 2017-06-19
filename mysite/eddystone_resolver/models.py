876bf3b2f929b39b078189af24a5e47c3480101c
Mike Walters <mike@flomp.net>
2017-06-19 22:42:07 +0100

    Do EID generation in model

--- mysite/eddystone_resolver/models.py
+++ mysite/eddystone_resolver/models.py
@@ -1,4 +1,11 @@
+import binascii
+
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.utils import timezone
+
+from eddystone_resolver.crypto import compute_eid
 
 class Beacon(models.Model):
     name = models.CharField(max_length=50)
@@ -10,6 +17,21 @@ class Beacon(models.Model):
     def __str__(self):
         return self.name
 
+    def counter(self, t=timezone.now()):
+        diff = int((t - self.reg_time).total_seconds())
+        c = (self.reg_counter + diff) & 0xFFFFFFFF
+        c = (c >> self.k) << self.k
+        return c
+
+    def counter_roll(self):
+        return (1 << self.k)
+
+    def counter_prev(self, t=timezone.now()):
+        return (self.counter(t) - self.counter_roll()) & 0xFFFFFFFF
+
+    def counter_next(self, t=timezone.now()):
+        return (self.counter(t) + self.counter_roll()) & 0xFFFFFFFF
+
 class EID(models.Model):
     beacon = models.ForeignKey(Beacon, on_delete=models.CASCADE)
     eid = models.CharField(max_length=16)
@@ -17,3 +39,8 @@ class EID(models.Model):
 
     def __str__(self):
         return self.eid
+
+@receiver(pre_save, sender=EID)
+def generate_eid(sender, instance, *args, **kwargs):
+    eid_bytes = compute_eid(instance.beacon.key, instance.beacon.k, instance.clock)
+    instance.eid = binascii.hexlify(eid_bytes).decode("ascii")
