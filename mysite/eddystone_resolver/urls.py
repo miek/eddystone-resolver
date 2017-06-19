c3c4288ff133a7264d08431f94f598c1ec64405d
Mike Walters <mike@flomp.net>
2017-06-19 17:31:16 +0100

    Add beacon lookup

--- mysite/eddystone_resolver/urls.py
+++ mysite/eddystone_resolver/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^beacon/$', views.beacons, name='beacons'),
+    url(r'^beacon/(?P<beacon_name>[A-Za-z0-9 _-]+)/$', views.beacon, name='beacon'),
 ]
--- mysite/eddystone_resolver/views.py
+++ mysite/eddystone_resolver/views.py
@@ -1,4 +1,16 @@
-from django.http import HttpResponse
+from django.forms.models import model_to_dict
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404
+
+from .models import Beacon
 
 def index(request):
     return HttpResponse("Hello world")
+
+def beacons(request):
+    return JsonResponse([model_to_dict(beacon) for beacon in Beacon.objects.all()], safe=False)
+
+def beacon(request, beacon_name):
+    if request.method == 'GET':
+        beacon = get_object_or_404(Beacon, name=beacon_name)
+        return JsonResponse(model_to_dict(beacon))
