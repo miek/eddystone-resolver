36fffb39185fc4232cbc733343075ed1087505c3
Mike Walters <mike@flomp.net>
2017-06-19 14:07:02 +0100

    Add initial resolver models

--- mysite/eddystone_resolver/models.py
+++ mysite/eddystone_resolver/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Beacon(models.Model):
+    name = models.CharField(max_length=50)
+    key = models.BinaryField()
+    reg_time = models.DateTimeField()
+    reg_counter = models.IntegerField()
+    k = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.name
+
+class EID(models.Model):
+    beacon = models.ForeignKey(Beacon, on_delete=models.CASCADE)
+    eid = models.CharField(max_length=16)
+    clock = models.IntegerField()
+
+    def __str__(self):
+        return self.eid
