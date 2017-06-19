be69a3b7efdd143fb81748b9d60c88233df9fc5a
Mike Walters <mike@flomp.net>
2017-06-19 19:24:41 +0100

    Add basic EID resolution

--- mysite/eddystone_resolver/urls.py
+++ mysite/eddystone_resolver/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^beacon/$', views.beacons, name='beacons'),
     url(r'^beacon/(?P<beacon_name>[A-Za-z0-9 _-]+)/$', views.beacon, name='beacon'),
+    url(r'^eid/(?P<beacon_eid>[A-Fa-f0-9]{16})/$', views.resolve_eid, name='resolve_eid'),
 ]
--- mysite/eddystone_resolver/views.py
+++ mysite/eddystone_resolver/views.py
@@ -2,7 +2,7 @@ from django.forms.models import model_to_dict
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404
 
-from .models import Beacon
+from .models import Beacon, EID
 
 def index(request):
     return HttpResponse("Hello world")
@@ -14,3 +14,7 @@ def beacon(request, beacon_name):
     if request.method == 'GET':
         beacon = get_object_or_404(Beacon, name=beacon_name)
         return JsonResponse(model_to_dict(beacon))
+
+def resolve_eid(request, beacon_eid):
+    eid = get_object_or_404(EID, eid=beacon_eid)
+    return JsonResponse(model_to_dict(eid.beacon))
